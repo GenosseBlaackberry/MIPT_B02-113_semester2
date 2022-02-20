94f297532f00441efceeed8cdc4c525a654e068c
GenosseBlaackberry <51916553+GenosseBlaackberry@users.noreply.github.com>
2022-02-21 04:46:20 +0700

    Create 1.cpp

---
+++ laba/1.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+unsigned int max_x, max_y, N;
+unsigned int x_cors[10000], y_cors[10000];
+bool stopped[10000] = { 0 };
+
+void input(unsigned int &max_x, unsigned int &max_y, unsigned int &N, 
+			unsigned int x_cors[], unsigned int y_cors[]) {
+	using namespace std;
+	cin >> max_x >> max_y;
+	cin >> N;
+	for (unsigned int i = 0; i < N; i++) {
+		cin >> x_cors[i];
+		cin >> y_cors[i];
+	}
+	return;
+}
+
+void iteration(unsigned int& max_x, unsigned int& max_y, unsigned int& N,
+	unsigned int x_cors[], unsigned int y_cors[], bool stopped[]) {
+	for (unsigned int i = 0; i < N; i++) {
+		unsigned int j = 0;
+		while (stopped[i] == 0) {
+			j++;
+			short way = rand() % 4;
+			if (way == 0 and x_cors[i] < max_x) {
+				x_cors[i]++;
+				if (x_cors[i] == max_x) {
+					stopped[i] = 1;
+				}
+			}
+			else if (way == 1) {
+				x_cors[i]--;
+				if (x_cors[i] == 0) {
+					stopped[i] = 1;
+				}
+			}
+			else if (way == 2) {
+				y_cors[i]++;
+				if (y_cors[i] == max_y) {
+					stopped[i] = 1;
+				}
+			}
+			else {
+				y_cors[i]--;
+				if (y_cors[i] == 0) {
+					stopped[i] = 1;
+				}
+			}
+		}
+		std::cout << j << std::endl;
+	}
+	return;
+}
+
+
+
+int main() {
+	input(max_x, max_y, N, x_cors, y_cors);
+	iteration(max_x, max_y, N, x_cors, y_cors, stopped);
+	return 0;
+}
