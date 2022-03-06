ca26201c9e6f382f03536f10855fead8ef08612a
GenosseBlaackberry <51916553+GenosseBlaackberry@users.noreply.github.com>
2022-03-07 03:09:31 +0700

    Create 3.cpp

---
+++ seminar4/3.cpp
@@ -0,0 +1,21 @@
+void third_iteration(int &number, int &sum) {
+	using namespace std;
+	unsigned int temp;
+	cin >> temp;
+	if (temp == 0) {
+		return;
+	}
+	number++;
+	sum += temp;
+	third_iteration(number, sum);
+}
+
+void third() {
+	double average;
+	int number = 0;
+	int sum = 0;
+	third_iteration(number, sum);
+	average = double(sum) / double(number);
+	std::cout << average;
+	return;
+}
