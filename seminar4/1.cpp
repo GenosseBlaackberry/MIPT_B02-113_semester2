9e514a2a0c5f5a2e823225d407780c51b0fe6bee
GenosseBlaackberry <51916553+GenosseBlaackberry@users.noreply.github.com>
2022-03-07 03:08:54 +0700

    Create 1.cpp

---
+++ seminar4/1.cpp
@@ -0,0 +1,13 @@
+void first() {
+	unsigned int temp;
+	using namespace std;
+	cin >> temp;
+	if (temp == 0) {
+		return;
+	}
+	if (temp % 2 != 0) {
+		cout << temp << endl;
+		cout << endl;
+	}
+	first();
+}
