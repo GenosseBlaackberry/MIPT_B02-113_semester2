ddd90162da258fa0506192a5fe61e9bffffb524b
GenosseBlaackberry <51916553+GenosseBlaackberry@users.noreply.github.com>
2022-03-07 03:09:13 +0700

    Create 2.cpp

---
+++ seminar4/2.cpp
@@ -0,0 +1,15 @@
+void second() {
+	unsigned int temp1, temp2;
+	using namespace std;
+	cin >> temp1;
+	if (temp1 == 0) {
+		return;
+	}
+	cout << temp1 << endl;
+	cout << endl;
+	cin >> temp2;
+	if (temp2 == 0) {
+		return;
+	}
+	second();
+}
