adb6d28c8777915432520015149ac6aae38ef900
GenosseBlaackberry <51916553+GenosseBlaackberry@users.noreply.github.com>
2022-03-07 03:10:13 +0700

    Create 5.cpp

---
+++ seminar4/5.cpp
@@ -0,0 +1,18 @@
+void fifth_iteration(unsigned int n) {
+	unsigned int temp;
+	using namespace std;
+	temp = n % 10;
+	if (n == 0) {
+		return;
+	}
+	n = (n - temp) / 10;
+	cout << temp;
+	fifth_iteration(n);
+}
+
+void fifth() {
+	unsigned int n;
+	std::cin >> n,
+	fifth_iteration(n);
+	return;
+}
