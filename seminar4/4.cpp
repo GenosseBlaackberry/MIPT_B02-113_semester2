c2e3ba43dd22e919a87250028e611994372dc8a1
GenosseBlaackberry <51916553+GenosseBlaackberry@users.noreply.github.com>
2022-03-07 03:09:54 +0700

    Create 4.cpp

---
+++ seminar4/4.cpp
@@ -0,0 +1,25 @@
+void forth_iteration(unsigned int& counter, unsigned int& max) {
+	using namespace std;
+	unsigned int temp;
+	cin >> temp;
+	if (temp == 0) {
+		return;
+	}
+	if (temp > max) {
+		max = temp;
+		counter = 1;
+	}
+	else if (temp == max) {
+		counter++;
+	}
+	forth_iteration(counter, max);
+	return;
+}
+
+void forth() {
+	unsigned int counter = 0;
+	unsigned int max = 0;
+	forth_iteration(counter, max);
+	std::cout << counter;
+	return;
+}
