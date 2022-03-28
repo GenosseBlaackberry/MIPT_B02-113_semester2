fe402de2b67b05dbfa2883283cb123d077c68133
GenosseBlaackberry <51916553+GenosseBlaackberry@users.noreply.github.com>
2022-03-29 00:05:47 +0700

    Create 3.cpp

---
+++ seminar6/3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void my_personal_swap(int *a, int *b) {
+	using namespace std;
+	int *temp = new int;
+	if (a == nullptr || b == nullptr || temp == nullptr) {
+		cout << "Error in memory alocation";
+		delete temp;
+		return;
+	}
+	*temp = *a;
+	*a = *b;
+	*b = *temp;
+	delete temp;
+	return;
+}
+
+int third() {
+	using namespace std;
+	int a, b;
+	cin >> a >> b;
+	my_personal_swap(&a, &b);
+	cout << a << " " << b << endl;
+	my_personal_swap(&a, nullptr);
+	my_personal_swap(nullptr, &b);
+	my_personal_swap(nullptr, nullptr);
+	return 0;
+}
+
+int main() {
+	third();
+	return 0;
+}
